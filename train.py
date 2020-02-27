4e257f1ca514e3fe881fa867d563b4385042a113
Aadesh Magare <aadeshmagare01@gmail.com>
2020-02-28 01:20:42 +0530

    moved print to the end of epoc

--- train.py
+++ train.py
@@ -55,13 +55,7 @@ def train(epoch):
 #             if 'bias' in name:
 #                 writer.add_scalar('LastLayerGradients/grad_norm2_bias', para.grad.norm(), n_iter)
 
-        print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
-            loss.item(),
-            optimizer.param_groups[0]['lr'],
-            epoch=epoch,
-            trained_samples=batch_index * args.b + len(images),
-            total_samples=len(cifar100_training_loader.dataset)
-        ))
+    print(f"Training Epoch: {epoch}\tLoss: {loss.item()}\tLR: {optimizer.param_groups[0]['lr']}")
 
         #update training loss for each iteration
 #         writer.add_scalar('Train/loss', loss.item(), n_iter)
