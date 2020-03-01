88490f69c6d553a3f40591bc31f9d5eaa4743964
Aadesh Magare <aadeshmagare01@gmail.com>
2020-03-01 17:43:16 +0530

    fixed coping script

--- runner.py
+++ runner.py
@@ -4,14 +4,13 @@ src_dir = 'cifar100-generated'
 tgt_dir = 'cifar100-original'
 no_of_fakes = [100, 200, 300, 400, 500]
 for i, n in enumerate(no_of_fakes):
-    for dir in os.listdir(src_dir):
-        if os.path.isdir(dir):
-            s = os.path.join(src_dir, dir)
-            d = os.path.join(tgt_dir, dir)
-        
-            files = sorted(os.listdir(s))[n-100:n]
-
+    for d in os.listdir(src_dir):
+        src = os.path.join(src_dir, d)
+        dst = os.path.join(tgt_dir, d)
+        if os.path.isdir(src):
+            files = sorted(os.listdir(src))[n-100:n]
+            files = [os.path.join(src, f) for f in files]
             for f in files:
-                shutil.copy(f, d)
-
+                shutil.copy(f, dst)
+    print(f'Copied {n} generated images')
     os.system(f'python train.py -net resnet50 | tee output_resnet50_augdata{n}.txt')
